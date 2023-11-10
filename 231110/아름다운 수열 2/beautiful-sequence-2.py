1cfb38bd0e1ffee8df07228bcf0c81bafc7e37e2
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2023-11-10 18:17:26 +0900

    Title: 아름다운 수열 2, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 231110/아름다운 수열 2/beautiful-sequence-2.py
+++ 231110/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,13 +1,9 @@
-from itertools import permutations
 n, m = map(int, input().split())
 a = list(input().split())
 b = list(input().split())
-tmp = list(set(list(permutations(b, m))))
 cnt = 0
-for t in tmp:
-    t = ''.join(list(t))
-    a = ''.join(a)
-    for i in range(len(a)-len(t)+1):
-        if t == a[i:i+len(t)]:
-            cnt += 1
+for i in range(n-m+1):
+    sub = a[i:i+m]
+    if sorted(sub) == sorted(b):
+        cnt+= 1
 print(cnt)
