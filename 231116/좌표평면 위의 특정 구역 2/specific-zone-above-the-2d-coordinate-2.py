994ce3746190e4ed8b5180e97e4a8892861e593b
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2023-11-16 09:56:38 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231116/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -0,0 +1,19 @@
+n = int(input())
+lst = []
+for _ in range(n):
+    x, y = map(int, input().split())
+    lst.append((x, y))
+
+for i in range(n):
+    mn_area = float('inf')
+    mx_x, mx_y = float('-inf'), float('-inf')
+    mn_x, mn_y = float('inf'), float('inf')
+    for j in range(n):
+        if i == j:
+            continue
+        x, y = lst[j]
+        mx_x, mx_y = max(mx_x, x), max(mx_y, y)
+        mn_x, mn_y = min(mn_x, x), min(mn_y, y)
+    area = (mx_x-mn_x)*(mx_y-mn_y)
+    mn_area = min(mn_area, area)
+print(mn_area)
