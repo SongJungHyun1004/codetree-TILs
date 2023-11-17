077ea1acf7b945f8bac8b7ea1447d63e08d914a9
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2023-11-17 11:05:46 +0900

    Title: 겹치지 않는 선분 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231117/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -0,0 +1,17 @@
+n = int(input())
+lst = []
+for _ in range(n):
+    x1, x2 = map(int, input().split())
+    lst.append((x1, x2))
+lst = sorted(lst)
+stack = []
+for l in lst:
+    if stack:
+        if stack[-1] < l[1]:
+            stack.append(l[1])
+            continue
+        while stack[-1] > l[1]:
+            stack.pop()
+    else:
+        stack.append(l[1])
+print(len(stack))
