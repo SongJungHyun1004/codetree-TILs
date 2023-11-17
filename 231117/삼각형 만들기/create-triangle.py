95209a0998f497353668fd626a2897bd0e683b27
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2023-11-17 09:49:54 +0900

    Title: 삼각형 만들기, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231117/삼각형 만들기/create-triangle.py
@@ -0,0 +1,17 @@
+n = int(input())
+lst = []
+for _ in range(n):
+    x, y = map(int, input().split())
+    lst.append((x, y))
+mx = -1
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            x1, y1 = lst[i]
+            x2, y2 = lst[j]
+            x3, y3 = lst[k]
+            if (x1 != x2 and x1 != x3) and (y1 != y2 and y1 != y3):
+                continue
+            s = abs((x1*y2+x2*y3+x3*y1)-(x2*y1+x3*y2+x1*y3))
+            mx = max(mx, s)
+print(mx) if mx != -1 else print(0)
