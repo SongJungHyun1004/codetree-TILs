6c51833468283da9a1c0ad470fe047703bb23d0c
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2023-11-18 11:20:45 +0900

    Title: 수 선택 순서, Time: 213ms, Memory: 25MB, Status: Passed - CodeTree

--- 231118/수 선택 순서/number-selection-order.py
+++ 231118/수 선택 순서/number-selection-order.py
@@ -1,11 +1,14 @@
 from collections import deque
 n, x = map(int, input().split())
 arr = list(enumerate(list(map(int, input().split()))))
+for i in range(n):
+    idx, value = arr[i]
+    arr[i] = (value, idx)
 q = deque(arr)
 cnt = 0
 while True:
-    p, v = q[0]
-    if q.index(max(q)) == 0:
+    v, p = q[0]
+    if max(q)[0] == v:
         q.popleft()
         cnt += 1
         if x == p:
