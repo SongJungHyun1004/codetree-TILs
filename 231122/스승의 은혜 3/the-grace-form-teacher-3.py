4c3fa2bb218206ea9f23336144ec57faad6a581d
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2023-11-22 10:32:38 +0900

    Title: 스승의 은혜 3, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231122/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,14 @@
+n, b = map(int, input().split())
+prices = []
+for _ in range(n):
+    p, s = map(int, input().split())
+    prices.append((int(p)//2, s))
+prices = [(0,0)]+sorted(prices)
+cnt = 0
+total = 0
+for i in range(1, n+1):
+    total += prices[i-1][0]+prices[i][0]+prices[i][1]-prices[i-1][1]
+    if total > b:
+        break
+    cnt += 1
+print(cnt)
