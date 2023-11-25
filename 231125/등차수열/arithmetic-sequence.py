a9810599e6f12b61581bc611567ae2a6a1fab3b2
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2023-11-25 11:34:13 +0900

    Title: 등차수열, Time: 109ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 231125/등차수열/arithmetic-sequence.py
+++ 231125/등차수열/arithmetic-sequence.py
@@ -1,7 +1,7 @@
 n = int(input())
-arr = sorted(list(map(int, input().split())))
+arr = list(map(int, input().split()))
 cnt = 0
-for k in range(arr[0], arr[-1]+1):
+for k in range(min(arr), max(arr)+1):
     for i in range(n):
         for j in range(i+1, n):
             if arr[j] - k == k - arr[i]:
