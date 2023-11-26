11bb938988fbee50608081cc56eb5d537b1c4e2a
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2023-11-26 10:51:02 +0900

    Title: 3개의 선 2, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231126/3개의 선 2/three-lines-2.py
@@ -0,0 +1,19 @@
+n = int(input())
+MAX = 10
+lst = []
+for _ in range(n):
+    x, y = map(int, input().split())
+    lst.append((x, y))
+def canPassAll():
+    xys = [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1], [1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]]
+    for i in range(MAX+1):
+        for j in range(MAX+1):
+            for k in range(MAX+1):
+                for xy1, xy2, xy3 in xys:
+                    ans = 1
+                    for p in lst:
+                        if p[xy1] == i or p[xy2] == j or p[xy3] == k:
+                            continue
+                            ans = 0
+                        return ans
+print(canPassAll())
