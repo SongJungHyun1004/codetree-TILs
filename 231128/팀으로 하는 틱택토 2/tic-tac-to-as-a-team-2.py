99937cd28809ddda7fd8abddf8cba9e59f3dbe03
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2023-11-28 10:35:18 +0900

    Title: 팀으로 하는 틱택토 2, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 231128/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 231128/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -2,15 +2,17 @@ ttt = [
     list(input())
     for _ in range(3)
 ]
-cnt = 0
+cnt = set()
 for i in range(3):
     if len(set(ttt[i])) == 2:
-        cnt += 1
+        cnt.add(tuple(sorted(ttt[i])))
     tmp = [ttt[j][i] for j in range(3)]
     if len(set(tmp)) == 2:
-        cnt += 1
-if len(set([ttt[0][0], ttt[1][1], ttt[2][2]])) == 2:
-    cnt += 1
-if len(set([ttt[0][2], ttt[1][1], ttt[2][0]])) == 2:
-    cnt += 1
-print(cnt)
+        cnt.add(tuple(sorted(tmp)))
+tmp2 = [ttt[0][0], ttt[1][1], ttt[2][2]]
+tmp3 = [ttt[0][2], ttt[1][1], ttt[2][0]]
+if len(set(tmp2)) == 2:
+    cnt.add(tuple(sorted(tmp2)))
+if len(set(tmp3)) == 2:
+    cnt.add(tuple(sorted(tmp3)))
+print(len(cnt))
