e89da9c059879f2b429454784fccc63bd42cad81
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2023-11-29 14:35:28 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 150ms, Memory: 25MB, Status: Passed - CodeTree

--- 231129/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 231129/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -1,10 +1,15 @@
 n = int(input())
 s = input()
-
+def myCount(tmp, length):
+    cnt = 0
+    for i in range(n-length+1):
+        if s[i:i+length] == tmp:
+            cnt += 1
+    return cnt
 for i in range(1, n+1):
     flag = 1
-    for j in range(n-i):
-        if s.count(s[j:j+i]) > 1:
+    for j in range(n-i+1):
+        if myCount(s[j:j+i], i) > 1:
             flag = 0
             break
     if flag:
