d4eb30533b7f5f38b2655e44657dcf31ea73fdf0
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2023-12-01 10:06:23 +0900

    Title: 숫자들의 최대 차, Time: 316ms, Memory: 25MB, Status: Passed - CodeTree

--- 231201/숫자들의 최대 차/maximum-difference-in-numbers.py
+++ 231201/숫자들의 최대 차/maximum-difference-in-numbers.py
@@ -3,10 +3,10 @@ arr = []
 for _ in range(n):
     arr.append(int(input()))
 mx = float('-inf')
-for i in range(1, 10000):
+for i in range(1, max(arr)+1):
     lst = []
     for a in arr:
-        if i <= a <= i+3:
+        if i <= a <= i+k:
             lst.append(a)
     mx = max(mx, len(lst))
 print(mx)
