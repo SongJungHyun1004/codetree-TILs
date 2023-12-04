c7ba12d446c6c6a915934f2cafbfb98f0d6c232b
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2023-12-04 14:12:50 +0900

    Title: 초기 수열 복원하기, Time: 1337ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 231204/초기 수열 복원하기/restore-initial-sequence.py
+++ 231204/초기 수열 복원하기/restore-initial-sequence.py
@@ -1,19 +1,17 @@
+from itertools import permutations
+
 n = int(input())
 arr = list(map(int, input().split()))
-cnt = []
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        if i+j != arr[0] or i == j:
-            continue
-        for k in range(1, n+1):
-            if j+k != arr[1] or k == j or k == i:
-                continue
-            for l in range(1, n+1):
-                if k+l != arr[2] or l == k or l== j or l == i:
-                    continue
-                for m in range(1, n+1):
-                    if l+m != arr[3] or m == l or m == k or m == j or m == i:
-                        continue
-                    cnt.append([i, j, k, l, m])
-cnt = sorted(cnt)
-print(*cnt[0])
+lst = [i for i in range(1, n+1)]
+tmp = []
+tmp += list(permutations(lst, n))
+for i in tmp:
+    perm = list(i)
+    flag = 1
+    for j in range(1, n):
+        if perm[j-1] + perm[j] != arr[j-1]:
+            flag = 0
+            break
+    if flag:
+        print(*perm)
+        break
