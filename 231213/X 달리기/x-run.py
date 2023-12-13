8f55957b0e0d7b9bf5807645224a692bf798c250
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2023-12-13 20:20:42 +0900

    Title: X 달리기, Time: 175ms, Memory: 73MB, Status: Passed - CodeTree

---
+++ 231213/X 달리기/x-run.py
@@ -0,0 +1,15 @@
+x = int(input())
+t = 0
+dist = 0
+v = 1
+while dist != x:
+    dist += v
+    if x-dist >= (v+1)*(v+2)//2:
+        v += 1
+    elif x-dist >= v*(v+1)//2:
+        v = v
+    else:
+        v -= 1
+    t += 1
+
+print(t)
