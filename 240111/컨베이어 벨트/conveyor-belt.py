d514c9968865ae8d7bfb9aa8ca17e292e4fb8828
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-01-11 13:41:28 +0900

    Title: 컨베이어 벨트, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240111/컨베이어 벨트/conveyor-belt.py
+++ 240111/컨베이어 벨트/conveyor-belt.py
@@ -4,8 +4,5 @@ b = list(map(int, input().split()))
 arr = a+b
 for _ in range(t):
     arr = [arr[-1]]+arr[:-1]
-for i in range(n):
-    print(arr[i],end=' ')
-print()
-for i in range(n,2*n):
-    print(arr[i],end=' ')
+print(*arr[:n])
+print(*arr[n:])
