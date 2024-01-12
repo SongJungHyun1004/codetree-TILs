938a789249129b9f5a339bd2ad25138e4777df26
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-01-12 11:09:04 +0900

    Title: 1차원 젠가, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240112/1차원 젠가/jenga-1d.py
+++ 240112/1차원 젠가/jenga-1d.py
@@ -2,7 +2,7 @@ n = int(input())
 arr = []
 for _ in range(n):
     arr.append(int(input()))
-arr = arr[::-1]
+
 for _ in range(2):
     s, e = map(int, input().split())
     s-=1;e-=1
@@ -12,5 +12,5 @@ for _ in range(2):
             tmp.append(arr[i])
     arr = tmp[:]
 print(len(arr))
-for i in arr[::-1]:
+for i in arr:
     print(i)
