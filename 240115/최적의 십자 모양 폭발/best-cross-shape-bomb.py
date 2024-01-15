eaf7fe74d1477180fffb742a2551ec07c5f41e68
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-01-15 12:53:15 +0900

    Title: 최적의 십자 모양 폭발, Time: 369ms, Memory: 27MB, Status: Wrong Answer - CodeTree

---
+++ 240115/최적의 십자 모양 폭발/best-cross-shape-bomb.py
@@ -0,0 +1,58 @@
+import copy
+n = int(input())
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+tmp = []
+
+dxs = [0,-1,0,1]
+dys = [1,0,-1,0]
+def in_range(x, y):
+    return 0<=x<n and 0<=y<n
+
+def explode(x, y):
+    v = tmp[x][y]
+    tmp[x][y] = 0
+    for dx, dy in zip(dxs, dys):
+        for i in range(1, v):
+            nx, ny = x + dx * i, y + dy * i
+            if in_range(nx, ny):
+                tmp[nx][ny] = 0
+
+def drop():
+    for j in range(n):
+        t = [0]*n
+        k = 0
+        for i in range(n-1, -1, -1):
+            if tmp[i][j]:
+                t[k] = tmp[i][j]
+                k += 1
+        for i, v in enumerate(t[::-1]):
+            tmp[i][j] = v
+
+def countPair():
+    cnt = 0
+    for i in range(n-1):
+        for j in range(n-1):
+            if tmp[i][j] and (tmp[i][j] == tmp[i+1][j] or tmp[i][j] == tmp[i][j+1]):
+                cnt += 1
+    t = [tmp[i][n-1] for i in range(n)]
+    for i in range(n-1):
+        if t[i] and t[i] == t[i+1]:
+            cnt += 1
+    t = tmp[n-1][:]
+    for i in range(n-1):
+        if t[i] and t[i] == t[i+1]:
+            cnt += 1
+    return cnt
+
+mx = 0
+for i in range(n):
+    for j in range(n):
+        tmp = copy.deepcopy(grid)
+        explode(i, j)
+        drop()
+        mx = max(mx, countPair())
+
+print(mx)
