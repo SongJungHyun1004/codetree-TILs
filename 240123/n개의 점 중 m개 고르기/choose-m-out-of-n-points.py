8851eda52b6411d3d4bdcd83297479165a348646
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-01-23 12:36:22 +0900

    Title: n개의 점 중 m개 고르기, Time: 417ms, Memory: 28MB, Status: Passed - CodeTree

--- 240123/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
+++ 240123/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -1,4 +1,3 @@
-import math
 n, m = map(int, input().split())
 dots = []
 for _ in range(n):
@@ -13,7 +12,7 @@ def get_dist(arr):
         for j in range(i+1, m):
             dot1 = dots[arr[i]]
             dot2 = dots[arr[j]]
-            d = math.sqrt(abs(dot1[0]-dot2[0])**2+abs(dot1[1]-dot2[1])**2)
+            d = abs(dot1[0]-dot2[0])**2+abs(dot1[1]-dot2[1])**2
             mx = max(mx, d)
     return mx
 
@@ -31,4 +30,4 @@ def combi(idx, cnt):
     combi(idx+1, cnt)
 
 combi(0, 0)
-print(int(mn**2))
+print(mn)
