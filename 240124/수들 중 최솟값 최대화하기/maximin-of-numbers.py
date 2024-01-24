eb5fe65348f7c6b7ccca05203c8f9f5bdd931e6e
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-01-24 16:13:08 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 5031ms, Memory: 27MB, Status: Time Limit Exceed - CodeTree

---
+++ 240124/수들 중 최솟값 최대화하기/maximin-of-numbers.py
@@ -0,0 +1,35 @@
+n = int(input())
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+row_visited = [False]*n
+col_visited = [False]*n
+pos_lst = []
+mx = 0
+
+def get_min(pos_lst):
+    mn = float('inf')
+    for x, y in pos_lst:
+        mn = min(mn, grid[x][y])
+    return mn
+
+def combi(i):
+    global mx
+    if i == n:
+        mx = max(mx, get_min(pos_lst))
+        return
+    for x in range(n):
+        for y in range(n):
+            if row_visited[x] or col_visited[y]:
+                continue
+            pos_lst.append((x, y))
+            row_visited[x] = True
+            col_visited[y] = True
+            combi(i+1)
+            pos_lst.pop()
+            row_visited[x] = False
+            col_visited[y] = False
+            
+combi(0)
+print(mx)
