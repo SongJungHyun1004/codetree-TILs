c9cfcd7e73dca28315b41044470ded1d3404e2b9
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-01-24 16:41:55 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 3279ms, Memory: 28MB, Status: Passed - CodeTree

--- 240124/수들 중 최솟값 최대화하기/maximin-of-numbers.py
+++ 240124/수들 중 최솟값 최대화하기/maximin-of-numbers.py
@@ -8,7 +8,7 @@ grid = [
 row_visited = [False]*n
 col_visited = [False]*n
 pos_lst = []
-mx = 0
+mx = -1
 
 def get_min(pos_lst):
     mn = float('inf')
@@ -16,10 +16,12 @@ def get_min(pos_lst):
         mn = min(mn, grid[x][y])
     return mn
 
-def combi(i):
+def combi(i, curr_min):
     global mx
+    if curr_min <= mx:  # Pruning
+        return
     if i == n:
-        mx = max(mx, get_min(pos_lst))
+        mx = max(mx, curr_min)
         return
     for x in range(n):
         if row_visited[x]:
@@ -27,13 +29,13 @@ def combi(i):
         for y in range(n):
             if col_visited[y]:
                 continue
-            pos_lst.append((x, y))
             row_visited[x] = True
             col_visited[y] = True
-            combi(i+1)
-            pos_lst.pop()
+            pos_lst.append((x, y))
+            combi(i+1, min(curr_min, grid[x][y]))  # Update current minimum
             row_visited[x] = False
             col_visited[y] = False
-            
-combi(0)
+            pos_lst.pop()
+
+combi(0, float('inf'))
 print(mx)
