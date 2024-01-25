5e43d94d3b2fef7c9ec0d5db6cb1245b5dc6ac02
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-01-25 11:05:20 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 5091ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

---
+++ 240125/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,26 @@
+import sys
+sys.setrecursionlimit(10**5)
+n, m = map(int, input().split())
+miro = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+visited = [
+    [False]*m
+    for _ in range(n)
+]
+dxs = [0, 1]
+dys = [1, 0]
+
+def in_range(x, y):
+    return 0<=x<n and 0<=y<m
+
+def dfs(x, y):
+    visited[x][y] = True
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and miro[nx][ny]:
+            dfs(nx, ny)
+
+dfs(0, 0)
+print(1) if visited[n-1][m-1] else print(0)
