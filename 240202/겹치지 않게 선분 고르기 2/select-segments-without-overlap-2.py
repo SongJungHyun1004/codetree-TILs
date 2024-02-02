207b0316107372f0e3711fdfa0e58eec97932005
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-02-02 16:54:11 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 181ms, Memory: 25MB, Status: Passed - CodeTree

--- 240202/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
+++ 240202/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -3,6 +3,7 @@ lst = [(0, 0)]
 for _ in range(n):
     x1, x2 = map(int, input().split())
     lst.append((x1, x2))
+lst = sorted(lst)
 dp = [0]+[-1]*n
 for i in range(1, n+1):
     for j in range(i):
