c7e300ed3d1754c4d6eb0c16183134d7e986c446
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-02-06 22:32:45 +0900

    Title: 배낭 채우기, Time: 205ms, Memory: 32MB, Status: Passed - CodeTree

--- 240206/배낭 채우기/knapsack.py
+++ 240206/배낭 채우기/knapsack.py
@@ -1,14 +1,14 @@
 n, m = map(int, input().split())
-arr = []
+arr = [(0,0)]
 for _ in range(n):
     w, v = map(int, input().split())
     arr.append((w, v))
 dp = [
     [0]*(m+1)
-    for _ in range(n)
+    for _ in range(n+1)
 ]
 
-for i in range(n):
+for i in range(1, n+1):
     for j in range(1, m+1):
         weight = arr[i][0]
         value = arr[i][1]
@@ -16,4 +16,4 @@ for i in range(n):
             dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight]+value)
         else:
             dp[i][j] = dp[i-1][j]
-print(dp[n-1][m])
+print(dp[n][m])
