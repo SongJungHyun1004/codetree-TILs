300e42923204de1f10a00a5229457eb8c6cdfd7c
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-02-06 21:50:42 +0900

    Title: 부분 수열의 합, Time: 168ms, Memory: 32MB, Status: Passed - CodeTree

--- 240206/부분 수열의 합/the-sum-of-the-subsequences.py
+++ 240206/부분 수열의 합/the-sum-of-the-subsequences.py
@@ -7,9 +7,9 @@ dp = [
 dp[0][0] = True
 
 for i in range(1, n+1):
-    for j in range(1, m+1):
+    for j in range(m+1):
         if j>=arr[i] and dp[i-1][j-arr[i]]:
             dp[i][j] = True
-        if dp[i-1][j]:
+        elif dp[i-1][j]:
             dp[i][j] = True
 print('Yes') if dp[n][m] else print('No')
