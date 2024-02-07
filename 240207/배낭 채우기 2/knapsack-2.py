be2b775c5e981da0fd6ef1576a0ecf09b4251505
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-02-07 17:56:41 +0900

    Title: 배낭 채우기 2, Time: 131ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240207/배낭 채우기 2/knapsack-2.py
+++ 240207/배낭 채우기 2/knapsack-2.py
@@ -9,8 +9,8 @@ for i in range(1, m+1):
     for j in range(n):
         weight = arr[j][0]
         value = arr[j][1]
-        if dp[i-weight] == -1:
-            continue
         if i >= weight:
+            if dp[i-weight] == -1:
+                continue
             dp[i] = max(dp[i], dp[i-1], dp[i-weight]+value)
 print(dp[m])
