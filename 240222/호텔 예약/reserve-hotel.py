9f9272c7ce0dc9f43b255f0ebc15951f44440705
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-02-22 11:43:03 +0900

    Title: 호텔 예약, Time: 907ms, Memory: 36MB, Status: Passed - CodeTree

--- 240222/호텔 예약/reserve-hotel.py
+++ 240222/호텔 예약/reserve-hotel.py
@@ -4,7 +4,7 @@ for _ in range(n):
     s, e = map(int, input().split())
     reservation.append((s, +1))
     reservation.append((e, -1))
-reservation = sorted(reservation)
+reservation = sorted(reservation, key=lambda x:(x[0], -x[1]))
 cnt = 0
 mx = 0
 for _, v in reservation:
