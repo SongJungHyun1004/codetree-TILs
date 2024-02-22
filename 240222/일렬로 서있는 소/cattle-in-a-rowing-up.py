b8e3457da29aaa80548f37ea527579a300ec330d
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-02-22 20:15:35 +0900

    Title: 일렬로 서있는 소, Time: 5013ms, Memory: 26MB, Status: Time Limit Exceed - CodeTree

--- 240222/일렬로 서있는 소/cattle-in-a-rowing-up.py
+++ 240222/일렬로 서있는 소/cattle-in-a-rowing-up.py
@@ -1,18 +1,26 @@
-def combinations(array, r):
-    for i in range(len(array)):
-        if r == 1:
-            yield [array[i]]
-        else:
-            for next in combinations(array[i+1:], r-1):
-                yield [array[i]] + next
-
 n = int(input())
-pos = sorted([int(input()) for _ in range(n)])
-
+pos = []
+for _ in range(n):
+    pos.append(int(input()))
+pos = sorted(pos)
+selected = [False]*n
 cnt = 0
-for cows in combinations(pos, 3):
-    x, y, z = cows
-    if x < y < z and y-x <= z-y <= 2*(y-x):
-        cnt += 1
+def combi(idx, last):
+    global cnt
+    if idx == 3:
+        xyz = []
+        for i in range(n):
+            if selected[i]:
+                xyz.append(pos[i])
+        x, y, z = xyz[0], xyz[1], xyz[2]
+        if x < y < z and y-x <= z-y <= 2*(y-x):
+            cnt += 1
+        return
+    for i in range(last, n):
+        if not selected[i]:
+            selected[i] = True
+            combi(idx+1, i)
+            selected[i] = False
 
+combi(0, 0)
 print(cnt)
