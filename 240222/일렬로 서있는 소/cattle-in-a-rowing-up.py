b7beb1fac5db4de88b6add29bfd85dcae1a26ede
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-02-22 20:49:05 +0900

    Title: 일렬로 서있는 소, Time: 1242ms, Memory: 25MB, Status: Passed - CodeTree

--- 240222/일렬로 서있는 소/cattle-in-a-rowing-up.py
+++ 240222/일렬로 서있는 소/cattle-in-a-rowing-up.py
@@ -1,26 +1,13 @@
-n = int(input())
-pos = []
-for _ in range(n):
-    pos.append(int(input()))
-pos = sorted(pos)
-selected = [False]*n
-cnt = 0
-def combi(idx, last):
-    global cnt
-    if idx == 3:
-        xyz = []
-        for i in range(n):
-            if selected[i]:
-                xyz.append(pos[i])
-        x, y, z = xyz[0], xyz[1], xyz[2]
-        if x < y < z and y-x <= z-y <= 2*(y-x):
-            cnt += 1
-        return
-    for i in range(last, n):
-        if not selected[i]:
-            selected[i] = True
-            combi(idx+1, i)
-            selected[i] = False
+def count_orders(n, cows):
+    cows.sort()
+    result = 0
+    for x in range(n):
+        for y in range(x + 1, n):
+            for z in range(y + 1, n):
+                if cows[y] - cows[x] <= cows[z] - cows[y] <= 2 * (cows[y] - cows[x]):
+                    result += 1
+    return result
 
-combi(0, 0)
-print(cnt)
+n = int(input().strip())
+cows = [int(input().strip()) for _ in range(n)]
+print(count_orders(n, cows))
