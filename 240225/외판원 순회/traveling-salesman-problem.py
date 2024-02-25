02850867f8a5b417b19ffc97269d6befcd6d0d9c
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-02-25 15:32:01 +0900

    Title: 외판원 순회, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240225/외판원 순회/traveling-salesman-problem.py
+++ 240225/외판원 순회/traveling-salesman-problem.py
@@ -7,19 +7,19 @@ grid = [
 visited = [False]*n
 cost = []
 mn = float('inf')
-def choose(i):
+def choose(i, last):
     global mn
-    if i == n:
-        mn = min(mn, sum(cost))
+    if i == n-1:
+        mn = min(mn, sum(cost)+grid[last][0])
         return
-    for j in range(n):
-        if i == j or visited[j]:
+    for j in range(1, n):
+        if last == j or not grid[last][j] or visited[j]:
             continue
-        cost.append(grid[i][j])
+        cost.append(grid[last][j])
         visited[j] = True
-        choose(i+1)
+        choose(i+1, j)
         cost.pop()
         visited[j] = False
 
-choose(0)
+choose(0, 0)
 print(mn)
