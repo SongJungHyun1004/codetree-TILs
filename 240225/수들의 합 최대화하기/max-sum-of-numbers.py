a24c1f19df8b408cb64cd44efd6df4ebb34a95b2
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-02-25 14:55:48 +0900

    Title: 수들의 합 최대화하기, Time: 288ms, Memory: 27MB, Status: Passed - CodeTree

--- 240225/수들의 합 최대화하기/max-sum-of-numbers.py
+++ 240225/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -4,23 +4,19 @@ grid = [
     for _ in range(n)
 ]
 num = []
-selected_i = [False]*n
 selected_j = [False]*n
 mx = 0
-def choose(idx):
+def choose(i):
     global mx
-    if idx == n:
+    if i == n:
         mx = max(mx, sum(num))
         return
-    for i in range(n):
-        for j in range(n):
-            if not selected_i[i] and not selected_j[j]:
-                num.append(grid[i][j])
-                selected_i[i] = True
-                selected_j[j] = True
-                choose(idx+1)
-                num.pop()
-                selected_i[i] = False
-                selected_j[j] = False
+    for j in range(n):
+        if not selected_j[j]:
+            num.append(grid[i][j])
+            selected_j[j] = True
+            choose(i+1)
+            num.pop()
+            selected_j[j] = False
 choose(0)
 print(mx)
