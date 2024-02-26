4fc9ed84b34d5274bd2268ebcc059017b665739a
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-02-26 12:41:48 +0900

    Title: 빙하, Time: 1382ms, Memory: 30MB, Status: Passed - CodeTree

--- 240226/빙하/glacier.py
+++ 240226/빙하/glacier.py
@@ -4,16 +4,17 @@ grid = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
-visited = [
-    [False]*m
-    for _ in range(n)
-]
+
 dxs = [0,1,0,-1]
 dys = [1,0,-1,0]
 def in_range(x, y):
     return 0<=x<n and 0<=y<m
 
 def bfs(x, y):
+    visited = [
+        [False]*m
+        for _ in range(n)
+    ]
     q = deque([(x, y)])
     visited[x][y] = True
     while q:
@@ -41,23 +42,11 @@ def is_done():
                 return False
     return True
 t = 0
-d = [0,1,2,3]
-i, j = 0, 0
-d = 0
-searched = set()
-for _ in range(n*m):
+while True:
     if is_done():
         break
-    if not visited[i][j] and grid[i][j] == 0:
-        bfs(i, j)
-        size = melt()
-        t += 1
-    searched.add((i, j))
-    ni, nj = i + dxs[d], j + dys[d]
-    if not in_range(ni, nj) or (ni, nj) in searched:
-        d = (d+1)%4
-        i, j = i + dxs[d], j + dys[d]
-    else:
-        i, j = ni, nj
+    bfs(0, 0)
+    size = melt()
+    t += 1
 
 print(t, size)
