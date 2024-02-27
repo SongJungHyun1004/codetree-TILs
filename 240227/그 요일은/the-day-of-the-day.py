e0c83dd94402568837c5853028b5df05c998815e
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-02-27 13:12:10 +0900

    Title: 그 요일은, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240227/그 요일은/the-day-of-the-day.py
+++ 240227/그 요일은/the-day-of-the-day.py
@@ -2,10 +2,13 @@ m1, d1, m2, d2 = map(int, input().split())
 weekday = input()
 days = [0,31,29,31,30,31,30,31,31,30,31,30,31]
 day = 0
-for i in range(m1+1, m2):
-    day += days[i]
-day += days[m1]-d1+1
-day += d2
+if m2 > m1:
+    for i in range(m1+1, m2):
+        day += days[i]
+    day += days[m1]-d1+1
+    day += d2
+else:
+    day = d2-d1+1
 weeks = ['Sun','Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 for i in range(7):
     if weeks[i] == weekday:
