84aeb0a7338e0f227b3805ef74725e7ec4a573c7
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-02-27 15:13:28 +0900

    Title: 진수 to 진수, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240227/진수 to 진수/transformation-of-number-system.py
+++ 240227/진수 to 진수/transformation-of-number-system.py
@@ -1,8 +1,11 @@
 a, b = map(int, input().split())
 n = input()
 n = int(n, a)
+dic = {
+    0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'
+}
 result = ''
 while n:
-    result = str(n%b) + result
+    result = dic[n%b] + result
     n //= b
 print(result)
