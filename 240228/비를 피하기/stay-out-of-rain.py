588eef47e8eea61d066b3ca6deb9851836dcd428
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-02-28 13:40:31 +0900

    Title: 비를 피하기, Time: 5017ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

---
+++ 240228/비를 피하기/stay-out-of-rain.py
@@ -0,0 +1,50 @@
+from collections import deque
+n, h, m = map(int, input().split())
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+ans = [
+    [0]*n
+    for _ in range(n)
+]
+dxs = [0,1,0,-1]
+dys = [1,0,-1,0]
+def in_range(x, y):
+    return 0<=x<n and 0<=y<n
+
+def bfs(sx, sy):
+    visited = [
+        [False]*n
+        for _ in range(n)
+    ]
+    dist = [
+        [0]*n
+        for _ in range(n)
+    ]
+    q = deque([(sx, sy)])
+    visited[sx][sy] = True
+    while q:
+        x, y = q.popleft()
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if in_range(nx, ny) and not visited[nx][ny] and grid[nx][ny] != 1:
+                visited[nx][ny] = True
+                dist[nx][ny] = dist[x][y] + 1
+                q.append((nx, ny))
+    mn_dist = float('inf')
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j] == 3 and visited[i][j]:
+                mn_dist = min(mn_dist, dist[i][j])
+    if mn_dist == float('inf'):
+        ans[sx][sy] = -1
+    else:
+        ans[sx][sy] = mn_dist
+
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 2:
+            bfs(i, j)
+for i in range(n):
+    print(*ans[i])
