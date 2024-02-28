b19ad253577745ce7799168cba2b1db87bb32e78
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-02-28 14:18:07 +0900

    Title: 비를 피하기, Time: 204ms, Memory: 27MB, Status: Passed - CodeTree

--- 240228/비를 피하기/stay-out-of-rain.py
+++ 240228/비를 피하기/stay-out-of-rain.py
@@ -4,6 +4,14 @@ grid = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
+visited = [
+    [False]*n
+    for _ in range(n)
+]
+dist = [
+    [0]*n
+    for _ in range(n)
+]
 ans = [
     [0]*n
     for _ in range(n)
@@ -13,34 +21,34 @@ dys = [1,0,-1,0]
 def in_range(x, y):
     return 0<=x<n and 0<=y<n
 
-def bfs(sx, sy):
-    visited = [
-        [False]*n
-        for _ in range(n)
-    ]
-    dist = [
-        [0]*n
-        for _ in range(n)
-    ]
-    q = deque([(sx, sy)])
-    visited[sx][sy] = True
-    mn_dist = -1
+def push(nx, ny, new_dist):
+    q.append((nx, ny))
+    visited[nx][ny] = True
+    dist[nx][ny] = new_dist
+
+def bfs():
     while q:
         x, y = q.popleft()
         for dx, dy in zip(dxs, dys):
             nx, ny = x + dx, y + dy
             if in_range(nx, ny) and not visited[nx][ny] and grid[nx][ny] != 1:
-                visited[nx][ny] = True
-                dist[nx][ny] = dist[x][y] + 1
-                q.append((nx, ny))
-                if grid[nx][ny] == 3:
-                    mn_dist = dist[nx][ny]
-                    return mn_dist
-    return mn_dist
-    
+                push(nx, ny, dist[x][y]+1)
+q = deque()
+three_pos = []
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 3:
+            three_pos.append((i, j))
+for i, j in three_pos:
+    push(i, j, 0)
+
+bfs()
 for i in range(n):
     for j in range(n):
         if grid[i][j] == 2:
-            ans[i][j] = bfs(i, j)
+            if not visited[i][j]:
+                ans[i][j] = -1
+            else:
+                ans[i][j] = dist[i][j]
 for i in range(n):
     print(*ans[i])
