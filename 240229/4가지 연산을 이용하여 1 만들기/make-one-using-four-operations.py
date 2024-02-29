8b8e80d65c23e9a30d5ebcd2e6d7eae2bd77616b
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-02-29 12:54:46 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 294ms, Memory: 70MB, Status: Passed - CodeTree

--- 240229/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 240229/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -1,15 +1,36 @@
-import sys
-INT_MAX = sys.maxsize
-N_MAX = 1000000
+from collections import deque
 n = int(input())
-dp = [INT_MAX]*(N_MAX+1)
-dp[0] = 1
-dp[1] = 0
-for i in range(2, N_MAX+1):
-    if i%2 == 0:
-        dp[i] = min(dp[i], dp[i//2]+1)
-    if i%3 == 0:
-        dp[i] = min(dp[i], dp[i//3]+1)
-    if i != N_MAX:
-        dp[i] = min(dp[i], min(dp[i-1], dp[i+1])+1)
-print(dp[n])
+MAX = 2*n+1 #연산과정 중 나올 수 있는 최대 숫자
+mn = n-1 #최대 연산 횟수로 초기화
+visited = [False]*(MAX+1)
+op = [0]*(MAX+1)
+def bfs(num):
+    q = deque([num])
+    while q:
+        cur = q.popleft()
+        if cur == 1:
+            return
+        nxt = cur+1
+        if nxt <= MAX and not visited[nxt]:
+            q.append(nxt)
+            visited[nxt] = True
+            op[nxt] = op[cur]+1
+        nxt = cur-1
+        if nxt > 1 and not visited[nxt]:
+            q.append(nxt)
+            visited[nxt] = True
+            op[nxt] = op[cur]+1
+        if cur % 2 == 0:
+            nxt = cur//2
+            if not visited[nxt]:
+                q.append(nxt)
+                visited[nxt] = True
+                op[nxt] = op[cur]+1
+        if cur % 3 == 0:
+            nxt = cur//3
+            if not visited[nxt]:
+                q.append(nxt)
+                visited[nxt] = True
+                op[nxt] = op[cur]+1
+bfs(n)
+print(op[1])
