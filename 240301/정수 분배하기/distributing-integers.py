b7a9f4f15df0056ca3d377130ed9a9b60aa28e48
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-03-01 12:33:26 +0900

    Title: 정수 분배하기, Time: 136ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240301/정수 분배하기/distributing-integers.py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+lst = []
+for _ in range(n):
+    lst.append(int(input()))
+
+def is_possible(k):
+    cnt = 0
+    for elem in lst:
+        if elem < k:
+            return False
+        cnt += elem//k
+    return cnt >= m
+
+def binary_search():
+    left = 1
+    right = 10000
+    mx = 0
+    while left <= right:
+        mid = (left+right)//2
+        if is_possible(mid):
+            left = mid+1
+            mx = max(mx, mid)
+        else:
+            right = mid-1
+    return mx
+
+print(binary_search())
