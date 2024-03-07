fa024fffe0ee6abe508ea92f27fca0ac5ccf9e02
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-03-07 12:14:06 +0900

    Title: 괄호 쌍 만들어주기, Time: 143ms, Memory: 27MB, Status: Passed - CodeTree

--- 240307/괄호 쌍 만들어주기/pair-parentheses.py
+++ 240307/괄호 쌍 만들어주기/pair-parentheses.py
@@ -1,10 +1,14 @@
 a = list(input())
-open = 0
-close = 0
-for i in range(len(a)-1):
+n = len(a)
+R = [0]*(n)
+for i in range(n-2, -1, -1):
+    string = ''.join(a[i:i+2])
+    R[i] = R[i+1]
+    if '))' == string:
+        R[i] += 1
+pair = 0
+for i in range(n-1):
     string = ''.join(a[i:i+2])
     if '((' == string:
-        open += 1
-    elif '))' == string:
-        close += 1
-print(open*close)
+        pair += R[i]
+print(pair)
