d78b1a3bdb5b1f767beee834b235bc70ff8d535a
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-03-08 15:05:50 +0900

    Title: 회의실 겹치지 않게 하기, Time: 507ms, Memory: 30MB, Status: Passed - CodeTree

---
+++ 240308/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+requests = []
+for _ in range(n):
+    s, e = map(int, input().split())
+    requests.append((s, e))
+requests = sorted(requests, key=lambda x: (x[1], x[0]))
+checked = 0
+last_e = -1
+for s, e in requests:
+    if last_e <= s:
+        checked += 1
+        last_e = e
+print(n-checked)
