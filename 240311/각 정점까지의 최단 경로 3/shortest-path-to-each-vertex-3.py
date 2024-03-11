53a47dfd9faaab266749e264f4407eae292964c2
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-03-11 17:19:21 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 232ms, Memory: 24MB, Status: Passed - CodeTree

--- 240311/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.py
+++ 240311/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.py
@@ -1,4 +1,4 @@
-import heapq as hq
+from heapq import heappop, heappush
 import sys
 INF = sys.maxsize
 input = sys.stdin.readline
@@ -10,20 +10,23 @@ graph = [
 for _ in range(m):
     s, e, v = map(int, input().split())
     graph[s].append((e, v))
-dist = [INF]*(n+1)
-dist[1] = 0
-pq = []
-for i in range(1, n+1):
-    hq.heappush(pq, (dist[i], i))
-while pq:
-    now_d, now = hq.heappop(pq)
-    if dist[now] < now_d:
-        continue
-    for nxt, nxt_d in graph[now]:
-        if dist[nxt] > now_d + nxt_d:
-            dist[nxt] = now_d + nxt_d
-            hq.heappush(pq, (dist[nxt], nxt))
 
+def dijkstra(src):
+    dist = [INF]*(n+1)
+    dist[src] = 0
+    pq = []
+    heappush(pq, (0, src))
+    while pq:
+        now_d, now = heappop(pq)
+        if dist[now] < now_d:
+            continue
+        for nxt, nxt_d in graph[now]:
+            if dist[nxt] > now_d+nxt_d:
+                dist[nxt] = now_d+nxt_d
+                heappush(pq, (dist[nxt], nxt))
+    return dist
+
+dist = dijkstra(1)
 for i in range(2, n+1):
     if dist[i] == INF:
         print(-1)
