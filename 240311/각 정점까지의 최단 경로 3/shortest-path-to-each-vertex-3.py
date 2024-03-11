89dc64e98736c4c9bb90547260bbc4d1f407e221
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-03-11 16:44:18 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240311/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.py
@@ -0,0 +1,25 @@
+import heapq as hq
+import sys
+INF = sys.maxsize
+n, m = map(int, input().split())
+graph = [
+    []
+    for _ in range(n+1)
+]
+for _ in range(m):
+    s, e, v = map(int, input().split())
+    graph[s].append((e, v))
+dist = [INF]*(n+1)
+dist[1] = 0
+pq = []
+for i in range(1, n+1):
+    hq.heappush(pq, (dist[i], i))
+while pq:
+    now_d, now = hq.heappop(pq)
+    for nxt, nxt_d in graph[now]:
+        if dist[nxt] > now_d + nxt_d:
+            dist[nxt] = now_d + nxt_d
+            hq.heappush(pq, (dist[nxt], nxt))
+
+for i in range(2, n+1):
+    print(dist[i])
