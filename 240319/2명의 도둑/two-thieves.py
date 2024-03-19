4942ce8866d658527e3794b819ea3e6a01a58f72
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-03-19 10:48:28 +0900

    Title: 2명의 도둑, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240319/2명의 도둑/two-thieves.py
@@ -0,0 +1,48 @@
+n, m, c = map(int, input().split())
+room = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+selected = []
+def choose(i, stuffs):
+    global value
+    if i == m:
+        new_value = 0
+        weight = 0
+        for j in selected:
+            weight += stuffs[j]
+            if weight > c:
+                return
+            new_value += stuffs[j]**2
+        value = max(value, new_value)
+        return
+    selected.append(i)
+    choose(i+1, stuffs)
+    selected.pop()
+    choose(i+1, stuffs)
+
+mx = 0
+for i in range(n):
+    for j in range(n-m+1):
+        stuffs1 = room[i][j:j+m]
+        for k in range(i, n):
+            if i == k:
+                for l in range(j+m, n-m+1):
+                    stuffs2 = room[k][l:l+m]
+                    value = 0
+                    choose(0, stuffs1)
+                    tmp = value
+                    value = 0
+                    choose(0, stuffs2)
+                    mx = max(mx, tmp+value)
+            else:
+                for l in range(j, n-m+1):
+                    stuffs2 = room[k][l:l+m]
+                    value = 0
+                    choose(0, stuffs1)
+                    tmp = value
+                    value = 0
+                    choose(0, stuffs2)
+                    mx = max(mx, tmp+value)
+        
+print(mx)
