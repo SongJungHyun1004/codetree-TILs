6df039e99d50523deb74aab845b5de42b28033fe
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-03-22 09:20:45 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 393ms, Memory: 34MB, Status: Passed - CodeTree

--- 240322/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240322/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,31 +1,35 @@
-from collections import deque
+import sys
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+
 n = int(input())
-grid = [
-    list(map(int, input().split()))
-    for _ in range(n)
-]
+grid = [list(map(int, input().split())) for _ in range(n)]
 
-dxs = [0,1,0,-1]
-dys = [1,0,-1,0]
+dxs = [0, 1, 0, -1]
+dys = [1, 0, -1, 0]
+
 def in_range(x, y):
-    return 0<=x<n and 0<=y<n
+    return 0 <= x < n and 0 <= y < n
 
-mx = 1
+# dp[x][y] = (x, y)에서 시작했을 때의 최대 길이
+dp = [[-1 for _ in range(n)] for _ in range(n)]
+
+def dfs(x, y):
+    # 이미 계산된 경우 바로 반환
+    if dp[x][y] != -1:
+        return dp[x][y]
+    
+    dp[x][y] = 1  # 자기 자신도 카운트
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and grid[nx][ny] > grid[x][y]:
+            dp[x][y] = max(dp[x][y], dfs(nx, ny) + 1)
+    
+    return dp[x][y]
+
+answer = 0
 for i in range(n):
     for j in range(n):
-        dp = [
-            [0]*n
-            for _ in range(n)
-        ]
-        dp[i][j] = 1
-        x, y = i, j
-        q = deque([(x, y)])
-        while q:
-            x, y = q.popleft()
-            mx = max(mx, dp[x][y])
-            for dx, dy in zip(dxs, dys):
-                nx, ny = x + dx, y + dy
-                if in_range(nx, ny) and grid[nx][ny] > grid[x][y]:
-                    q.append((nx, ny))
-                    dp[nx][ny] = max(dp[nx][ny], dp[x][y]+1)
-print(mx)
+        answer = max(answer, dfs(i, j))
+
+print(answer)
