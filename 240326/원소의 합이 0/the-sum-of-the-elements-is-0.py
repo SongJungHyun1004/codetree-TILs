30021477e4bf2e250be3dbf79d003078e105a9f3
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-03-26 11:04:04 +0900

    Title: 원소의 합이 0, Time: 5090ms, Memory: 33MB, Status: Time Limit Exceed - CodeTree

---
+++ 240326/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+c = list(map(int, input().split()))
+d = list(map(int, input().split()))
+group1 = {}
+group2 = {}
+
+for i in range(n):
+    for j in range(n):
+        s = a[i]+b[j]
+        if s in group1:
+            group1[s] += 1
+        else:
+            group1[s] = 1
+for i in range(n):
+    for j in range(n):
+        s = c[i]+d[j]
+        if s in group2:
+            group2[s] += 1
+        else:
+            group2[s] = 1
+cnt = 0
+for i in group1.keys():
+    for j in group2.keys():
+        if i + j == 0:
+            cnt += group1[i]*group2[j]
+print(cnt)
