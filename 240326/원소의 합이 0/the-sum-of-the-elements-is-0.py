dea0b68102824a7d80b7eb93f6ec5920b4662b11
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-03-26 11:10:33 +0900

    Title: 원소의 합이 0, Time: 310ms, Memory: 41MB, Status: Passed - CodeTree

--- 240326/원소의 합이 0/the-sum-of-the-elements-is-0.py
+++ 240326/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -1,28 +1,25 @@
 import sys
+from collections import defaultdict
+
 input = sys.stdin.readline
 n = int(input())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 c = list(map(int, input().split()))
 d = list(map(int, input().split()))
-group1 = {}
-group2 = {}
+
+group1 = defaultdict(int)
+cnt = 0
 
 for i in range(n):
     for j in range(n):
-        s = a[i]+b[j]
-        if s in group1:
-            group1[s] += 1
-        else:
-            group1[s] = 1
-        s2 = c[i]+d[j]
-        if s2 in group2:
-            group2[s2] += 1
-        else:
-            group2[s2] = 1
-cnt = 0
-for i in group1.keys():
-    for j in group2.keys():
-        if i + j == 0:
-            cnt += group1[i]*group2[j]
+        s = a[i] + b[j]
+        group1[s] += 1
+
+for i in range(n):
+    for j in range(n):
+        s2 = -(c[i] + d[j])  # c+d의 합의 음수 값을 직접 계산
+        if s2 in group1:  # group1에 s2의 음수 값이 존재하는지 확인
+            cnt += group1[s2]  # 존재한다면, 그 조합의 수만큼 cnt를 증가
+
 print(cnt)
