2c6f792b0d3c47da00445c47d99e1a2402c30ad9
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-03-28 09:51:18 +0900

    Title: 앞에서부터 삭제하기 2, Time: 162ms, Memory: 31MB, Status: Passed - CodeTree

--- 240328/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 240328/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -1,23 +1,14 @@
+from heapq import heappush
 n = int(input())
-arr = [0] + list(map(int, input().split()))
-
-# 왼쪽에서부터 누적합
-prefix = [0 for _ in range(n+1)]
-for i in range(1, n+1):
-    prefix[i] = prefix[i-1] + arr[i]
-
-# 오른쪽에서부터 최소값
-temp = 10_001
-postfix = [0 for _ in range(n+1)]
-for i in range(n, -1, -1):
-    postfix[i] = min(temp, arr[i])
-    temp = postfix[i]
-
-total = sum(arr)
+arr = list(map(int, input().split()))
+pq = []
+heappush(pq, arr[n-1])
+summation = pq[0]
 mx = 0
-for k in range(1, n-1):
-    summation = total - prefix[k] - postfix[k+1]
-    avg = summation / (n-k-1)
+for k in range(n-2, 0, -1):
+    heappush(pq, arr[k])
+    summation += arr[k]
+    avg = (summation-pq[0]) / (n-k-1)
     mx = max(mx, avg)
 
 print('%.2f' % mx)
