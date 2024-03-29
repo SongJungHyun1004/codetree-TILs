3069afdff41dbc6f479333ed9793c10745d8942a
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-03-29 10:44:44 +0900

    Title: 선분 위의 점, Time: 230ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240329/선분 위의 점/point-on-the-line-segment.py
@@ -0,0 +1,32 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+def lower_bound(target):
+    left = 0
+    right = n-1
+    mn = n
+    while left <= right:
+        mid = (left+right)//2
+        if arr[mid] >= target:
+            right = mid - 1
+            mn = min(mn, mid)
+        else:
+            left = mid + 1
+    return mn
+            
+def upper_bound(target):
+    left = 0
+    right = n-1
+    mn = n
+    while left <= right:
+        mid = (left+right)//2
+        if arr[mid] > target:
+            right = mid - 1
+            mn = min(mn, mid)
+        else:
+            left = mid + 1
+    return mn
+
+for _ in range(m):
+    s, e = map(int, input().split())
+    print(upper_bound(e) - lower_bound(s))
