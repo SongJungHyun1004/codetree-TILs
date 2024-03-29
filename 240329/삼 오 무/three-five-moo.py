02148c47882504190a3f7dba4a8159e732e84eb0
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-03-29 11:12:37 +0900

    Title: 삼 오 무, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240329/삼 오 무/three-five-moo.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+def count(mid):
+    cal = mid//3+mid//5-mid//15
+    return mid-cal
+
+def binary_search():
+    left = 1
+    right = 10**9
+    mn = right
+    while left <= right:
+        mid = (left+right)//2
+        if count(mid) >= n:
+            mn = min(mn, mid)
+            right = mid - 1
+        else:    
+            left = mid + 1
+    return mn
+print(binary_search())
