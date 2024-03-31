9839b25d1494d13401dfaf922e842e5a3a2a86f1
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-03-31 14:29:19 +0900

    Title: 중앙값, Time: 705ms, Memory: 35MB, Status: Passed - CodeTree

--- 240331/중앙값/median.py
+++ 240331/중앙값/median.py
@@ -1,23 +1,23 @@
 import sys
-import copy
 from heapq import heappush, heappop
 input = sys.stdin.readline
-
-def print_median(min_pq, i):
-    tmp = copy.deepcopy(min_pq)
-    for _ in range(i//2):
-        heappop(tmp)
-    print(tmp[0], end=' ')
-    
-
 
 t = int(input())
 for _ in range(t):
     m = int(input())
     arr = list(map(int, input().split()))
     min_pq = []
+    max_pq = []
     for i, elem in enumerate(arr):
-        heappush(min_pq, elem)
         if i%2 == 0:
-            print_median(min_pq, i)
+            heappush(min_pq, elem)
+        else:
+            heappush(max_pq, -elem)
+        if max_pq and min_pq[0] < -max_pq[0]:
+            a = heappop(min_pq)
+            b = -heappop(max_pq)
+            heappush(max_pq, -a)
+            heappush(min_pq, b)
+        if i%2 == 0:
+            print(min_pq[0], end=' ')
     print()
