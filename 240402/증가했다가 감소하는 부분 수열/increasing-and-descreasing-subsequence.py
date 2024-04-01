00e6ff0e11a5851abfba3fbee8c665e52cb282f5
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-04-02 08:55:25 +0900

    Title: 증가했다가 감소하는 부분 수열, Time: 305ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240402/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [1]*n
+for i in range(n):
+    for j in range(i+1, n):
+        if arr[i] < arr[j]:
+            dp[j] = max(dp[j], dp[i]+1)
+
+for i in range(n):
+    for j in range(i+1, n):
+        if arr[i] > arr[j]:
+            dp[j] = max(dp[j], dp[i]+1)
+print(max(dp))
