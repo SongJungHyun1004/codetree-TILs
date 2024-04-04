95a93850b9d78f65ceccda8ad2a6b7da64dbe28a
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-04-04 15:49:43 +0900

    Title: k번째로 작은 쌍의 합, Time: 849ms, Memory: 38MB, Status: Passed - CodeTree

--- 240404/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
+++ 240404/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
@@ -1,20 +1,26 @@
 from heapq import heappush, heappop
 import sys
+
+# 입력 받기
 input = sys.stdin.readline
 n, m, k = map(int, input().split())
 arr1 = sorted(list(map(int, input().split())))
 arr2 = sorted(list(map(int, input().split())))
+
+# 우선순위 큐 초기화
 pq = []
-for elem1 in arr1:
-    heappush(pq, elem1 + arr2[0])
-i, j = 0, 1
-while pq and k:
-    v = heappop(pq)
-    if k == 1:
-        break
-    heappush(pq, arr1[i]+arr2[j])
-    i += 1
-    if i == n:
-        i = 0; j += 1
+for i, elem1 in enumerate(arr1):
+    # (합, A의 인덱스, B의 인덱스)로 큐에 추가
+    heappush(pq, (elem1 + arr2[0], i, 0))
+
+# K번째까지 반복
+while pq and k > 0:
+    v, i, j = heappop(pq)
     k -= 1
-print(v)
+    # K번째 원소를 찾으면 종료
+    if k == 0:
+        print(v)
+        break
+    # B의 다음 원소가 있다면, 현재 A의 원소와 B의 다음 원소를 합한 새로운 쌍을 큐에 추가
+    if j + 1 < m:
+        heappush(pq, (arr1[i] + arr2[j + 1], i, j + 1))
