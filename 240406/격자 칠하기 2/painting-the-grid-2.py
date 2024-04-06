c27033e77e79c61a27535341dffe3f9f64af999b
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-04-06 15:11:39 +0900

    Title: 격자 칠하기 2, Time: 192ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240406/격자 칠하기 2/painting-the-grid-2.py
+++ 240406/격자 칠하기 2/painting-the-grid-2.py
@@ -1,36 +1,40 @@
+from collections import deque
+import math
 n = int(input())
 grid = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
-
-half = round(n*n/2)
+visited = [[False]*n for _ in range(n)]
+half = math.floor(n**2/2+0.5)
 dxs = [0,1,0,-1]
 dys = [1,0,-1,0]
 def in_range(x, y):
     return 0<=x<n and 0<=y<n
 
-def dfs(x, y, visited, depth, mid):
-    global size
-    if depth == mid:
-        return
+def bfs(x, y, mid):
+    size = 0
+    q = deque([(x, y)])
     visited[x][y] = True
-    size += 1
-    for dx, dy in zip(dxs, dys):
-        nx, ny = x + dx, y + dy
-        if in_range(nx, ny) and not visited[nx][ny] and abs(grid[nx][ny]-grid[x][y]) <= mid:
-            dfs(nx, ny, visited, depth+1, mid)
-            visited[nx][ny] = False
+    while q:
+        x, y = q.popleft()
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if in_range(nx, ny) and not visited[nx][ny] and abs(grid[nx][ny]-grid[x][y]) <= mid:
+                visited[nx][ny] = True
+                q.append((nx, ny))
+                size += 1
+    return size
 
 def isPossible(mid):
     global size
-    visited = [[False]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            visited[i][j] = False
     for i in range(n):
         for j in range(n):
             if not visited[i][j]:
-                size = 0
-                dfs(i, j, visited, 0, mid)
-                if half <= size:
+                if half <= bfs(i, j, mid):
                     return True
     return False
 
