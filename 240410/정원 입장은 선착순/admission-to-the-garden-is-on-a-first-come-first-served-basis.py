aa8fe7405cb2107a174f326f4214cc4569cf2879
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-04-10 11:41:09 +0900

    Title: 정원 입장은 선착순, Time: 259ms, Memory: 27MB, Status: Wrong Answer - CodeTree

---
+++ 240410/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
@@ -0,0 +1,29 @@
+from heapq import heappush, heappop
+import sys
+input = sys.stdin.readline
+MAX = 10**4
+n = int(input())
+pq = []
+for i in range(n):
+    a, t = map(int, input().split())
+    heappush(pq, (a, i, t))
+
+last = 0
+mx = 0   
+waiting = []
+while pq:
+    while last > pq[0][0]:
+        a, i, t = heappop(pq)
+        heappush(waiting, (i, a, t))
+        if not pq:
+            break
+    if not waiting:
+        a, _, t = heappop(pq)
+        last = a + t
+    else:
+        while waiting:
+            _, a, t = heappop(waiting)
+            wait = last - a
+            mx = max(mx, wait)
+            last += t
+print(mx)
