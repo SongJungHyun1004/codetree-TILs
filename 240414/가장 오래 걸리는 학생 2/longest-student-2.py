e152d536e6dc6e6e65ae100e3272aad7e213da07
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-04-14 15:36:51 +0900

    Title: 가장 오래 걸리는 학생 2, Time: 781ms, Memory: 36MB, Status: Wrong Answer - CodeTree

---
+++ 240414/가장 오래 걸리는 학생 2/longest-student-2.py
@@ -0,0 +1,30 @@
+from heapq import heappush, heappop
+import sys
+INF = sys.maxsize
+n, m = map(int, input().split())
+graph = [
+    []
+    for _ in range(n+1)
+]
+for _ in range(m):
+    i, j, d = map(int, input().split())
+    graph[i].append((j, d))
+    graph[j].append((i, d))
+
+def dijkstra(src):
+    dist = [INF]*(n+1)
+    dist[src] = 0
+    pq = []
+    heappush(pq, (0, src))
+    while pq:
+        now_d, now = heappop(pq)
+        if dist[now] < now_d:
+            continue
+        for nxt, nxt_d in graph[now]:
+            if dist[nxt] > now_d + nxt_d:
+                dist[nxt] = now_d + nxt_d
+                heappush(pq, (dist[nxt], nxt))
+    return dist[1:]
+
+dist = dijkstra(n)
+print(max(dist))
