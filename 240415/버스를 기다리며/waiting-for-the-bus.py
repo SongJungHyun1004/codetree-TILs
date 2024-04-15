60117d883a7c96ae0f6277682c6f653946e13b6b
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-04-15 11:50:49 +0900

    Title: 버스를 기다리며, Time: 167ms, Memory: 31MB, Status: Wrong Answer - CodeTree

---
+++ 240415/버스를 기다리며/waiting-for-the-bus.py
@@ -0,0 +1,30 @@
+n, m, c = map(int, input().split())
+arrive = sorted(list(map(int, input().split())))
+
+def isPossible(mid):
+    bus = 1
+    pre = arrive[0]
+    people = 1
+    for t in arrive[1:]:
+        if mid >= t-pre and people <= c:
+            people += 1
+        else:
+            pre = t
+            people = 1
+            bus += 1
+    return bus <= m
+
+def binary_search():
+    left = 0
+    right = 10**9
+    mn = right
+    while left <= right:
+        mid = (left+right)//2
+        if isPossible(mid):
+            mn = min(mn, mid)
+            right = mid - 1
+        else:
+            left = mid + 1
+    return mn
+
+print(binary_search())
