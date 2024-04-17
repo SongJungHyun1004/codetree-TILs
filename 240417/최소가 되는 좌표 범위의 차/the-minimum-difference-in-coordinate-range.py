5a3e76db47fb2c4f49abfab9f051fb265e571570
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-04-17 12:38:25 +0900

    Title: 최소가 되는 좌표 범위의 차, Time: 1198ms, Memory: 43MB, Status: Passed - CodeTree

--- 240417/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.py
+++ 240417/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.py
@@ -1,11 +1,14 @@
 from sortedcontainers import SortedSet
+import sys
+input = sys.stdin.readline
+MAX = sys.maxsize
 n, d = map(int, input().split())
 pos = []
 for _ in range(n):
     x, y = map(int, input().split())
     pos.append((x, y))
 pos = sorted(pos)
-mn = float('inf')
+mn = MAX
 treeset = SortedSet()
 j = 0
 for i in range(n):
@@ -14,4 +17,4 @@ for i in range(n):
         mn = min(mn, pos[i][0]-pos[j][0])
         treeset.remove((pos[j][1], pos[j][0]))
         j += 1
-print(mn) if mn != float('inf') else print(-1)
+print(mn) if mn != MAX else print(-1)
