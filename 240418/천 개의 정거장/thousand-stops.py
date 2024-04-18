8f00ff8491c41fcaf4d3c83e45763a008c55a210
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-04-18 16:49:12 +0900

    Title: 천 개의 정거장, Time: 1218ms, Memory: 54MB, Status: Passed - CodeTree

--- 240418/천 개의 정거장/thousand-stops.py
+++ 240418/천 개의 정거장/thousand-stops.py
@@ -4,29 +4,32 @@ input = sys.stdin.readline
 INF = sys.maxsize
 a, b, n = map(int, input().split())
 graph = [
-    []
+    [(INF, INF)]*1001
     for _ in range(1001)
 ]
 for bus in range(1, n+1):
     fare, pn = map(int, input().split())
     pass_points = list(map(int, input().split()))
-    pre = pass_points[0]
-    for nxt in pass_points[1:]:
-        graph[pre].append((nxt, bus, fare))
-        pre = nxt
+    for i in range(pn-1):
+        for j in range(i, pn):
+            x = pass_points[i]
+            y = pass_points[j]
+            graph[x][y] = min(graph[x][y], (fare, j-i))
 
 def dijkstra(src, dst):
     dist = [(INF, INF)]*1001
     dist[src] = (0, 0)
     pq = []
-    heappush(pq, (0, 0, -1, src))
+    heappush(pq, (0, 0, src))
     while pq:
-        now_d, now_t, now_bus, now = heappop(pq)
-        for nxt, nxt_bus, nxt_d in graph[now]:
-            add_fare = nxt_d if now_bus != nxt_bus else 0
-            if dist[nxt][0] > now_d + add_fare or (dist[nxt][0] == now_d + add_fare and dist[nxt][1] > now_t + 1):
-                dist[nxt] = (now_d+add_fare, now_t+1)
-                heappush(pq, (now_d+add_fare, now_t+1, nxt_bus, nxt))
+        now_d, now_t, now = heappop(pq)
+        if dist[now] != (now_d, now_t):
+            continue
+        for i in range(1, 1001):
+            nxt_dist = (graph[now][i][0]+now_d, graph[now][i][1]+now_t)
+            if dist[i][0] > nxt_dist[0] or (dist[i][0] == nxt_dist[0] and dist[i][1] > nxt_dist[1]):
+                dist[i] = nxt_dist
+                heappush(pq, (nxt_dist[0], nxt_dist[1], i))
     return dist[dst]
 
 mn_fare, mn_time = dijkstra(a, b)
