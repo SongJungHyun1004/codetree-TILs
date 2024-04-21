f4490d3da140b20203b0134e15d6c2359571a166
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-04-21 13:03:00 +0900

    Title: 우리 지금 만나 당장 만나, Time: 5096ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

---
+++ 240421/우리 지금 만나 당장 만나/meet-now-right-now.py
@@ -0,0 +1,35 @@
+import math
+n = int(input())
+x = list(map(int, input().split()))
+v = list(map(int, input().split()))
+info = list(zip(x, v))
+info.sort()
+
+def isPossible(mid):
+    first = info[0][0]
+    last = info[-1][0]
+    for p in range(first, last+1):
+        mx = -1
+        for x, v in info:
+            time = abs(x-p)/v
+            mx = max(mx, time)
+        if mx <= mid:
+            return True
+    return False
+
+def binary_search():
+    left = 0.00000
+    right = 1000000000.00000
+    mn = right
+    i = 0
+    while left <= right or i == 50:
+        mid = (left+right)//2
+        if isPossible(mid):
+            right = mid - 1
+            mn = min(mn, mid)
+        else:
+            left = mid + 1
+        i += 1
+    return format(mn, '.4f')
+
+print(binary_search())
