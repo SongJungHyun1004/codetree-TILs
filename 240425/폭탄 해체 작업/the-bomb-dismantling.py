7cc9e3dcc004c3e455cc0955de2ee76b172f6bef
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-04-25 14:37:46 +0900

    Title: 폭탄 해체 작업, Time: 274ms, Memory: 28MB, Status: Passed - CodeTree

--- 240425/폭탄 해체 작업/the-bomb-dismantling.py
+++ 240425/폭탄 해체 작업/the-bomb-dismantling.py
@@ -1,13 +1,20 @@
+from heapq import heappush, heappop
 n = int(input())
-lst = []
+bombs = []
 for _ in range(n):
     score, time = map(int, input().split())
-    lst.append((score, time))
-lst.sort(key = lambda x: (x[1], -x[0]))
+    bombs.append((score, time))
+bombs.sort(key = lambda x: x[1])
+m = bombs[-1][1]
+time_lst = [[] for _ in range(m+1)]
+for s, t in bombs:
+    time_lst[t].append(s)
+
 ans = 0
-last_t = 0
-for s, t in lst:
-    if t > last_t:
-        ans += s
-        last_t = t
+pq = []
+for t in range(m, 0, -1):
+    for s in time_lst[t]:
+        heappush(pq, -s)
+    if pq:
+        ans += -heappop(pq)
 print(ans)
