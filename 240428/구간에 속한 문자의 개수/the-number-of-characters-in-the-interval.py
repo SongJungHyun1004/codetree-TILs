5e4701ecf8c7709916876a842ef31ed1f61fb92a
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-04-28 11:58:43 +0900

    Title: 구간에 속한 문자의 개수, Time: 944ms, Memory: 172MB, Status: Passed - CodeTree

--- 240428/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
+++ 240428/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
@@ -7,15 +7,15 @@ mapper = {
     'c':2,
 }
 n, m, k = map(int, input().split())
-grid = [[[0, 0, 0] for _ in range(n+1)] for _ in range(n+1)]
+grid = [[[0, 0, 0] for _ in range(m+1)] for _ in range(n+1)]
 for i in range(1, n+1):
     string = list(input().rstrip())
     for j, s in enumerate(string, start=1):
         grid[i][j][mapper[s]] = 1
 
-prefix = [[[0, 0, 0] for _ in range(n+1)] for _ in range(n+1)]
+prefix = [[[0, 0, 0] for _ in range(m+1)] for _ in range(n+1)]
 for i in range(1, n+1):
-    for j in range(1, n+1):
+    for j in range(1, m+1):
         for kk in range(3):
             prefix[i][j][kk] = grid[i][j][kk] + prefix[i-1][j][kk] + prefix[i][j-1][kk] - prefix[i-1][j-1][kk]
 
