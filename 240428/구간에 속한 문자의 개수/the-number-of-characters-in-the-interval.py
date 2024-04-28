b543fbe9c7defea764f423a829312ae80e40a712
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-04-28 11:53:35 +0900

    Title: 구간에 속한 문자의 개수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240428/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+mapper = {
+    'a':0,
+    'b':1,
+    'c':2,
+}
+n, m, k = map(int, input().split())
+grid = [[[0, 0, 0] for _ in range(n+1)] for _ in range(n+1)]
+for i in range(1, n+1):
+    string = list(input().rstrip())
+    for j, s in enumerate(string, start=1):
+        grid[i][j][mapper[s]] = 1
+
+prefix = [[[0, 0, 0] for _ in range(n+1)] for _ in range(n+1)]
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        for k in range(3):
+            prefix[i][j][k] = grid[i][j][k] + prefix[i-1][j][k] + prefix[i][j-1][k] - prefix[i-1][j-1][k]
+
+for _ in range(k):
+    r1, c1, r2, c2 = map(int, input().split())
+    ans = []
+    for i in range(3):
+        ans.append(prefix[r2][c2][i] - prefix[r2][c1-1][i] - prefix[r1-1][c2][i] + prefix[r1-1][c1-1][i])
+    print(*ans)
