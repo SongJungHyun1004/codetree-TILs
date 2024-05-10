314ce96622e8d64bf74c047fedfc547f91819097
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-05-10 17:07:22 +0900

    Title: 트리 파악, Time: 1068ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240510/트리 파악/identifying-the-tree.py
+++ 240510/트리 파악/identifying-the-tree.py
@@ -1,21 +1,26 @@
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**5)
+
 n = int(input())
 tree = [[] for _ in range(n+1)]
 for _ in range(n-1):
     s, e = map(int, input().split())
     tree[s].append(e)
+    tree[e].append(s)
 
-def dfs(x):
-    global dist
+def dfs(x, depth):
+    if len(tree[x]) == 1 and x != 1:  # 리프 노드인 경우
+        return depth % 2  # 리프 노드에서 루트 노드까지의 깊이가 홀수인지 짝수인지 반환
+    total = 0
     visited[x] = True
     for nx in tree[x]:
         if not visited[nx]:
-            dist += 1
-            dfs(nx)
+            total += dfs(nx, depth + 1)
+    return total
 
-visited = [False]*(n+1)
-dist = 0
-dfs(1)
-print(1 if dist%2 else 0)
+visited = [False] * (n+1)
+# 루트 노드에서 시작하여 게임의 결과를 계산
+result = dfs(1, 0)
+# 결과가 홀수이면 a의 승리, 짝수이면 b의 승리
+print(1 if result % 2 else 0)
