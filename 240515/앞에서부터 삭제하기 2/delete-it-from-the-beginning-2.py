6cbd6c2ec8b3527b309ef1acff6eb6d0c44f748e
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-05-15 11:59:41 +0900

    Title: 앞에서부터 삭제하기 2, Time: 5101ms, Memory: 31MB, Status: Time Limit Exceed - CodeTree

--- 240515/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 240515/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -4,13 +4,16 @@ input = sys.stdin.readline
 
 n = int(input())
 arr = list(map(int, input().split()))
-mx = 0
-for k in range(n-3):
-    pq = []
-    for i in range(k+1, n):
-        heappush(pq, arr[i])
-    heappop(pq)
+arr = arr[::-1]
+pq = []
+heappush(pq, arr[0])
+heappush(pq, arr[1])
+mx = arr[0]
+for k in range(2, n):
+    heappush(pq, arr[k])
+    tmp = heappop(pq)
     avg = sum(pq)/len(pq)
     mx = max(mx, avg)
+    heappush(pq, tmp)
 
 print(f'{mx:.2f}')
