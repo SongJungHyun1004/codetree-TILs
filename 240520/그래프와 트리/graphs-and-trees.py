7d5e0d2c0cefb92ef0b1f7c45b3a3f7bedec3167
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-05-20 16:51:37 +0900

    Title: 그래프와 트리, Time: 272ms, Memory: 32MB, Status: Passed - CodeTree

--- 240520/그래프와 트리/graphs-and-trees.py
+++ 240520/그래프와 트리/graphs-and-trees.py
@@ -1,30 +1,29 @@
+import sys
+sys.setrecursionlimit(10**5)
+
 n, m = map(int, input().split())
-uf = [i for i in range(n+1)]
-
-def find(x):
-    if x == uf[x]:
-        return x
-    return find(uf[x])
-
-def union(a, b):
-    x = find(a)
-    y = find(b)
-    if x > y:
-        uf[x] = y
-    else:
-        uf[y] = x
-
-cycle = set()
+graph = [[] for _ in range(n+1)]
 for _ in range(m):
     s, e = map(int, input().split())
-    if find(s) != find(e):
-        union(s, e)
-    else:
-        cycle.add(find(s))
+    graph[s].append(e)
+    graph[e].append(s)
 
-uf = uf[1:]
-tree = set()
-for i in uf:
-    if i not in cycle:
-        tree.add(i)
-print(len(tree))
+def dfs(x):
+    global edge, node
+    node += 1
+    visited[x] = True
+    for nx in graph[x]:
+        edge += 1
+        if not visited[nx]:
+            dfs(nx)
+
+cnt = 0
+visited = [False]*(n+1)
+for i in range(1, n+1):
+    if not visited[i]:
+        edge, node = 0, 0
+        dfs(i)
+        if edge//2 == node-1:
+            cnt += 1
+
+print(cnt)
