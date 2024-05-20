ad2f9a85b40cdf1e25d5b9d579accb3582eb89ab
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-05-20 11:50:59 +0900

    Title: 트리 간선의 길이, Time: 5092ms, Memory: 31MB, Status: Time Limit Exceed - CodeTree

---
+++ 240520/트리 간선의 길이/length-of-tree-trunk.py
@@ -0,0 +1,21 @@
+n = int(input())
+tree = [[] for _ in range(n+1)]
+for _ in range(n-1):
+    s, e, d = map(int, input().split())
+    tree[s].append((e, d))
+    tree[e].append((s, d))
+
+def dfs(x, dist):
+    global ans
+    ans = max(ans, dist)
+    visited[x] = True
+    for nx, nd in tree[x]:
+        if not visited[nx]:
+            dfs(nx, dist+nd)
+    visited[x] = False
+
+ans = 0
+for i in range(1, n+1):
+    visited = [False]*(n+1)
+    dfs(i, 0)
+print(ans)
