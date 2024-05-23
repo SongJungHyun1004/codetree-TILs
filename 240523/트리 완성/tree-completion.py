9bdc9563f119400f332a43e4aa92c47d38b8e3bb
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-05-23 10:44:57 +0900

    Title: 트리 완성, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240523/트리 완성/tree-completion.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+uf = [i for i in range(n+1)]
+
+def find(x):
+    if x == uf[x]:
+        return x
+    return find(uf[x])
+
+def union(a, b):
+    x = find(a)
+    y = find(b)
+    if x != y:
+        uf[x] = y
+
+cut = 0
+for _ in range(m):
+    s, e = map(int, input().split())
+    if find(s) != find(e):
+        union(s, e)
+    else:
+        cut += 1
+
+connect = 0
+parents = [find(1)]
+for i in range(2, n+1):
+    if not find(i) in parents:
+        parents.append(find(i))
+        connect += 1
+
+print(cut+connect)
