2e2ec14719d871cecff46619119dedd329a3fd62
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-05-25 12:42:08 +0900

    Title: 특정한 정점과 연결하지 않기, Time: 907ms, Memory: 50MB, Status: Passed - CodeTree

--- 240525/특정한 정점과 연결하지 않기/do-not-connect-with-a-specific-node.py
+++ 240525/특정한 정점과 연결하지 않기/do-not-connect-with-a-specific-node.py
@@ -2,30 +2,34 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-uf = [i for i in range(n+1)]
+uf = [[i, 1] for i in range(n+1)]
 
 def find(x):
-    if x == uf[x]:
+    if x == uf[x][0]:
         return x
-    uf[x] = find(uf[x])
-    return uf[x]
+    uf[x][0] = find(uf[x][0])
+    return uf[x][0]
 
 def union(a, b):
     x = find(a)
     y = find(b)
     if x != y:
-        uf[x] = y
+        uf[x][0] = y
+        uf[y][1] += uf[x][1]
 
 for _ in range(m):
     i, j = map(int, input().split())
     union(i, j)
 
-a, b, k = map(int, input().split())
+lst = []
 for i in range(1, n+1):
+    lst.append((i, uf[find(i)][1]))
+lst.sort(key=lambda x:-x[1])
+
+a, b, k = map(int, input().split())
+for i, size in lst:
     if k == 0:
         break
-    if i == a:
-        continue
     if find(i) != find(a) and find(i) != find(b):
         union(i, a)
         k -= 1
