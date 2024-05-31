f2b25bf07ecf0e659b09ee81f6891be8446cc455
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-05-31 14:11:00 +0900

    Title: 친구의 키, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240531/친구의 키/height-of-friends.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visited = [False]*(n+1)
+order = []
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+def dfs(x):
+    visited[x] = True
+    for nx in graph[x]:
+        if not visited[nx]:
+            dfs(nx)
+    order.append(x)
+
+for i in range(1, n+1):
+    if not visited[i]:
+        dfs(i)
+
+print(*order[::-1])
