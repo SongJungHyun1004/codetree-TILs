532c08c69c8df23f6e4d86d1a72e77391b0f8538
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-06-01 08:57:49 +0900

    Title: 뿌요뿌요, Time: 210ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240601/뿌요뿌요/puyo-puyo.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(10**5)
+
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False]*n for _ in range(n)]
+dxs = [0,1,0,-1]
+dys = [1,0,-1,0]
+
+def in_range(x, y):
+    return 0<=x<n and 0<=y<n
+
+def dfs(x, y, num):
+    global size
+    visited[x][y] = True
+    size += 1
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and not visited[nx][ny] and grid[nx][ny] == num:
+            dfs(nx, ny, num)
+            
+block, mx_size = 0, 0
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            size = 0
+            dfs(i, j, grid[i][j])
+            if size >= 4:
+                block += 1
+            mx_size = max(mx_size, size)
+print(block, mx_size)
