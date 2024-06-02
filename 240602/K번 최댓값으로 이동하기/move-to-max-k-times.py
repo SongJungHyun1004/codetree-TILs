5d13f4fd90ddae87e88dba70da4b1e489097d007
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-06-02 15:00:41 +0900

    Title: K번 최댓값으로 이동하기, Time: 160ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240602/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+n, k = map(int, input().split())
+grid = [[0]*(n+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    grid[i][1:] = list(map(int, input().split()))
+r, c = map(int, input().split())
+
+dxs = [0,1,0,-1]
+dys = [1,0,-1,0]
+
+def in_range(x, y):
+    return 0<x<=n and 0<y<=n
+
+def bfs(x, y):
+    visited = [[False]*(n+1) for _ in range(n+1)]
+    q = deque([(x, y, grid[x][y])])
+    visited[x][y] = True
+    mx_val = 0
+    mx_x, mx_y = -1, -1
+    while q:
+        x, y, val = q.popleft()
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if in_range(nx, ny) and not visited[nx][ny] and grid[nx][ny] < val:
+                visited[nx][ny] = True
+                if mx_val < grid[nx][ny]:
+                    mx_val = grid[nx][ny]
+                    mx_x, mx_y = nx, ny
+                elif mx_val == grid[nx][ny]:
+                    if nx < mx_x:
+                        mx_x, mx_y = nx, ny
+                    elif nx == mx_x and ny < mx_y:
+                        mx_y = ny
+                q.append((nx, ny, val))
+
+    return mx_x, mx_y
+
+for _ in range(k):
+    r, c = bfs(r, c)
+    if (r, c) == (-1, -1):
+        break
+print(r, c)
