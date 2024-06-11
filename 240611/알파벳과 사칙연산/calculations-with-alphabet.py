447bc03bca508803304d254542de0910f91aced2
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-06-11 15:25:57 +0900

    Title: 알파벳과 사칙연산, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240611/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -0,0 +1,59 @@
+expr = input()
+var = {}
+index = {}
+idx = 0
+for s in expr:
+    if s.isalpha() and s not in var:
+        var[s] = 0
+        index[idx] = s
+        idx += 1
+n = len(var)
+
+def convert():
+    stack = []
+    result = ''
+    for s in expr:
+        if s.isalpha():
+            result += s
+        else:
+            if stack:
+                result += stack.pop()
+                stack.append(s)
+            else:
+                stack.append(s)
+    result += stack.pop()
+    return result
+
+def cal(a, b, op):
+    if op == '-':
+        return a-b
+    elif op == '+':
+        return a+b
+    elif op == '*':
+        return a*b
+        
+def calculate():
+    stack = []
+    for s in expr2:
+        if s.isalpha():
+            stack.append(var[s])
+        else:
+            b = stack.pop()
+            a = stack.pop()
+            stack.append(cal(a, b, s))
+    return stack[0]
+
+def choose(i):
+    global mx
+    if i == n:
+        mx = max(mx, calculate())
+        return
+    for nn in range(1, 5):
+        var[index[i]] = nn
+        choose(i+1)
+        var[index[i]] = 0
+
+expr2 = convert()
+mx = 0   
+choose(0)
+print(mx)
