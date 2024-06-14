59d51a11f36211e0d14a884f36380d7d55b0d526
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-06-14 12:15:18 +0900

    Title: 세 수의 합, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240614/세 수의 합/sum-of-three-num.py
@@ -0,0 +1,15 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+dic = {}
+cnt = 0
+for i in range(n-1):
+    for j in range(i+1, n):
+        s = arr[i]+arr[j]
+        if k-s in dic:
+            cnt += dic[k-s]
+        if s in dic:
+            dic[s] += 1
+        else:
+            dic[s] = 1
+print(cnt)
