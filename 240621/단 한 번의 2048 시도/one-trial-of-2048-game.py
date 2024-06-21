d5f5d8c9332f113b3e008317ead30fdf441fc677
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-06-21 17:41:37 +0900

    Title: 단 한 번의 2048 시도, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240621/단 한 번의 2048 시도/one-trial-of-2048-game.py
@@ -0,0 +1,51 @@
+grid = [
+    list(map(int, input().split()))
+    for _ in range(4)
+]
+d = input()
+
+def rotateL(box):
+    tmp = [[0]*4 for _ in range(4)]
+    for i in range(4):
+        for j in range(4):
+            tmp[i][j] = box[j][4-1-i]
+    return tmp
+
+def rotateR(box):
+    tmp = [[0]*4 for _ in range(4)]
+    for i in range(4):
+        for j in range(4):
+            tmp[i][j] = box[4-1-j][i]
+    return tmp
+
+def drop(box):
+    tmp = [[0]*4 for _ in range(4)]
+    for j in range(4):
+        k = 3
+        for i in range(3, -1, -1):
+            if i and grid[i][j] == grid[i-1][j]:
+                tmp[k][j] = 2*grid[i][j]
+                grid[i-1][j] = 0
+                k -= 1
+            elif grid[i][j]:
+                tmp[k][j] = grid[i][j]
+                k -= 1
+    return tmp
+
+if d == 'L':
+    grid = rotateL(grid)
+    grid = drop(grid)
+    grid = rotateR(grid)
+elif d == 'R':
+    grid = rotateR(grid)
+    grid = drop(grid)
+    grid = rotateL(grid)
+elif d == 'U':
+    grid = rotateL(rotateL(grid))
+    grid = drop(grid)
+    grid = rotateR(rotateR(grid))
+else:
+    grid = drop(grid)
+
+for i in range(4):
+    print(*grid[i])
