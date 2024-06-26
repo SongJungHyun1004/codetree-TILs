740b785537546d32fbd9b5fe632f3da93ef22f4e
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-06-26 11:44:49 +0900

    Title: bit 계산, Time: 329ms, Memory: 27MB, Status: Passed - CodeTree

--- 240626/bit 계산/bit-calculation.py
+++ 240626/bit 계산/bit-calculation.py
@@ -2,16 +2,16 @@ q = int(input())
 val = 0
 for _ in range(q):
     cmd = input().split()
-    if len(cmd) == 2:
+    if cmd[0] == 'clear':
+        val = 0
+    else:
         x = int(cmd[1])
         if cmd[0] == 'add':
-            val = val + (1 << x)
+            val |= (1 << x)
         elif cmd[0] == 'delete':
             if((val >> x) & 1):
-                val = val - (1 << x)
+                val -= (1 << x)
         elif cmd[0] == 'print':
             print((val >> x) & 1)
         elif cmd[0] == 'toggle':
-            val = val ^ (1 << x)
-    else:
-        val = 0
+            val ^= (1 << x)
