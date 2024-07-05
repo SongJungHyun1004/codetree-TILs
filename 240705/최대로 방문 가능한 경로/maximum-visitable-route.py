06af3cbe075dce553e01629f9b97383e55040d5c
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2024-07-05 19:09:42 +0900

    Title: 최대로 방문 가능한 경로, Time: 375ms, Memory: 36MB, Status: Passed - CodeTree

---
+++ 240705/최대로 방문 가능한 경로/maximum-visitable-route.py
@@ -0,0 +1,88 @@
+import sys
+from collections import deque
+
+INT_MIN = -sys.maxsize
+
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+edges = [[] for _ in range(n + 1)]
+
+# 진입차수를 관리합니다.
+indegree = [0] * (n + 1)
+
+# dist[i] : i번 노드까지의 최대 거리
+dist = [0] * (n + 1)
+
+# before[i] : i번 노드에서 최대 거리로 이동할 때
+# 다음으로 가야할 노드 번호
+before = [0] * (n + 1)
+
+# 위상정렬을 위한 큐를 관리합니다.
+q = deque()
+
+# 인접리스트로 관리합니다.
+for _ in range(m):
+    x, y = tuple(map(int, input().split()))
+   
+    # 사전순으로 가장 앞선 답을 찾기 위해
+    # 그래프를 역순으로 만들고, n번 노드에서 1번 노드로 가겠습니다.
+    edges[y].append(x)
+    indegree[x] += 1 # 진입차수를 갱신합니다.
+
+# 모든 거리를 매우 작은 값으로 초기화합니다.
+for i in range(1, n + 1):
+    dist[i] = INT_MIN
+
+# n번 노드만 거리를 0으로 해줍니다.
+dist[n] = 0
+
+# 처음 indegree 값이 0인 곳이 시작점이 됩니다.
+# 이 노드들을 queue에 넣어줍니다.
+for i in range(1, n + 1):
+    if not indegree[i]:
+        q.append(i)
+
+# 위상정렬을 진행합니다.
+# queue에 원소가 남아있다면 계속 진행합니다.
+while q:
+    # 가장 앞에 있는 원소를 뽑아줍니다.
+    x = q.popleft()
+
+    # x에서 갈 수 있는 모든 곳을 탐색합니다.
+    for y in edges[x]:
+        # dist 정보를 갱신합니다.
+        if dist[x] != INT_MIN:
+            if dist[y] < dist[x] + 1:
+                dist[y] = dist[x] + 1
+                before[y] = x;
+            # 값이 동일한 경우에는 
+            # 추후 사전순으로 앞선 답을 만들어주기 위해 더 작은 값을 넣어줍니다.
+            elif dist[y] == dist[x] + 1 and before[y] > x:
+                before[y] = x;
+
+        # 해당 노드의 indegree를 1만큼 감소시켜줍니다.
+        indegree[y] -= 1
+
+        # 비로소 indegree가 0이 되었다면
+        # queue에 새로 넣어줍니다.
+        if not indegree[y]:
+            q.append(y)
+
+# dist[1]에 값이 들어가 있지 않다면,
+# 1번 노드에서 n번 노드로 가는 길이 없습니다.
+if dist[1] == INT_MIN:
+    print(-1)
+    quit()
+
+# 1번 노드에서부터 before을 타고 가
+# n번 노드까지 경로를 저장합니다.
+cur = 1
+ans = [cur]
+while cur != n:
+    cur = before[cur]
+    ans.append(cur)
+
+# 정답과 경로를 출력합니다.
+print(len(ans))
+for num in ans:
+    print(num, end=" ")
