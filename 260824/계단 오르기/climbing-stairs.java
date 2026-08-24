a59b9b87ffb9490ee51ff9bc3c08a8efacd1815c
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2026-08-24 10:57:32 +0900

    [Passed] 계단 오르기 (128ms, 10MB)

---
+++ 260824/계단 오르기/climbing-stairs.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final int MOD = 10007;
+        int n = sc.nextInt();
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[2] = 1;
+        if(n == 2){
+            System.out.println(1);
+            System.exit(0);
+        }
+        for(int i = 3; i <= n; i++){
+            dp[i] = (dp[i-2]+dp[i-3])%MOD;
+        }
+        System.out.println(dp[n]);
+    }
+}
