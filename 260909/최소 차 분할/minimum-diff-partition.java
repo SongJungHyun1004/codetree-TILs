42cb14be10591fa683c1ee0780c85f1896cfcc3b
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2026-09-09 11:46:58 +0900

    [Passed] 최소 차 분할 (130ms, 10MB)

---
+++ 260909/최소 차 분할/minimum-diff-partition.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            total += arr[i];
+        }
+        int s = total/2;
+        boolean[] dp = new boolean[s+1];
+        dp[0] = true;
+        for(int i = 0; i < n; i++){
+            for(int j = s; j >= 1; j--){
+                if(j-arr[i] >= 0 && dp[j-arr[i]])
+                    dp[j] = true;
+            }
+        }
+        int mn = 0;
+        for(int i = s; i >= 1; i--){
+            if(dp[i]){
+                mn = Math.abs(i-(total-i));
+                break;
+            }
+        }
+        System.out.println(mn);
+    }
+}
