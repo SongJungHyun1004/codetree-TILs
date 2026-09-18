bf2f657fe292a15f255bd459b51bc1c7ac2cdacf
SongJungHyun1004 <44044105+SongJungHyun1004@users.noreply.github.com>
2026-09-18 21:09:44 +0900

    [Passed] 대칭 차집합 (1483ms, 73MB)

---
+++ 260918/대칭 차집합/symmetric-difference-set.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        Set<Integer> A = new HashSet<>();
+        Set<Integer> B = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            A.add(sc.nextInt());
+        }
+        for (int i = 0; i < m; i++) {
+            B.add(sc.nextInt());
+        }
+        Set<Integer> inter = new HashSet<>(A);
+        inter.retainAll(B);
+        System.out.println(A.size()+B.size()-2*inter.size());
+    }
+}
